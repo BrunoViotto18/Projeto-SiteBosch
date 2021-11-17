a7d88b88807aa53316104a779f32eb5d26735172
Bruno Viotto <brunoviottosantos@gmail.com>
2021-11-16 22:02:04 -0300

    Responsividade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function aumentarTamanho(){
+    document.getElementById('inputbusca').style.width="200px"
+    document.getElementById('inputbusca').style.backgroundColor="#999999"
+}
+
+function voltarTamanho(){
+    document.getElementById('inputbusca').style.width="57px"
+    document.getElementById('inputbusca').style.backgroundColor="#fff"
+}
+
+
 const menu = document.body.querySelector(".menu")
 const burger1 = document.body.querySelector("#burger1")
 const burger2 = document.body.querySelector("#burger2")
@@ -13,17 +24,3 @@ menu.addEventListener('click', () => {
     burger2.classList.toggle("burger2-js")
     burger3.classList.toggle("burger3-js")
 })
-
-maisDetalhesBosch.addEventListener('click', () => {
-    
-})
-
-function aumentarTamanho(){
-    document.getElementById('inputbusca').style.width="200px"
-    document.getElementById('inputbusca').style.backgroundColor="#999999"
-}
-
-function voltarTamanho(){
-    document.getElementById('inputbusca').style.width="57px"
-    document.getElementById('inputbusca').style.backgroundColor="#fff"
-}
